b9dbdba02bd53af41bfebecd36adb3d10a71a2ce
mooss <mooss@protonmail.com>
2018-02-02 17:43:52 +0100

    Example of part-of-speech analysis on SemStat's content.

--- example.py
+++ example.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
+import nltk
 import sys
 from semeval_util import xmlextract
-from textstat import frequency_analysis
+from textstat import *
 
 usage = "usage: " + sys.argv[0] + " file.xml"
 
@@ -27,20 +28,29 @@ extractor = xmlextract(source_filename)
 #     print('\trelated comment id:', relc.attrib['RELC_ID'])
 
 fulltext = extractor.get_all_text()
+joined_text = ''.join(fulltext)
 
 print( len(fulltext), ' entries')
+freq =  frequency_analysis( joined_text )
 
-freq =  frequency_analysis( ''.join(fulltext) )
+#print( "number of tokens found :\n\tby naive frequency analysis\t|\tby nltk's word_tokenize\n\t\t", len(freq), "\t\t\t|\t\t", len( nltk_frequency_analysis(joined_text) ))
 
 cardinal = 20
-
 print('here are the ', cardinal, ' most common words from ', source_filename, ':')
 
 count=1
-
 for el in sorted(freq, key=freq.__getitem__, reverse=True):
     if count <= cardinal:
         print('\t', el, '\t --> ', freq[el], ' occurences')
         count += 1
     else:
         break
+
+tagged_text = nltk.pos_tag( word_tokenize(joined_text) )
+tag_counter = defaultdict(int)
+for (word, tag) in tagged_text:
+    tag_counter[tag] += 1
+
+print('\n\noccurence of tags:')
+for el in sorted(tag_counter, key=tag_counter.__getitem__, reverse=True):
+    print('\t', el, '\t --> ', tag_counter[el])
