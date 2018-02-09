dca26cd63d223401f56f5bac202fce797e5f597e
mooss <mooss@protonmail.com>
2018-02-09 11:30:18 +0100

    Commented out tokenisation, added extraction of some questions.

--- example.py
+++ example.py
@@ -15,42 +15,57 @@ source_filename = sys.argv[1]
 
 extractor = xmlextract(source_filename)
 
+
+
 # for subject in extractor.get_org_subjects():
 #     print('\t', subject.text)
     
 # for body in extractor.get_org_bodies():
 #     print('\t', body.text)
 
-# for iden in extractor.get_org_questions_ids():
-#     print('\t', iden)
+question_ids=extractor.get_org_questions_ids()
+print("number of ids:", len(question_ids))
+
+lost=[4,8,15,16,23,42]
+my_selection = [ extractor.find_path_from_org_id('.', question_ids[offset] ) for offset in lost ]
+
+for question in my_selection:
+    print( question.find('./OrgQSubject').text,
+           '\n\t', question.find('./OrgQBody').text, '\n' )
 
 # for relc in extractor.get_rel_comments_from_org_id('Q268'):
 #     print('\trelated comment id:', relc.attrib['RELC_ID'])
 
-fulltext = extractor.get_all_text()
-joined_text = ''.join(fulltext)
+# fulltext = extractor.get_all_text()
+# joined_text = ''.join(fulltext)
 
-print( len(fulltext), ' entries')
-freq =  frequency_analysis( joined_text )
+# print( len(fulltext), ' entries')
+# freq =  frequency_analysis( joined_text )
 
-#print( "number of tokens found :\n\tby naive frequency analysis\t|\tby nltk's word_tokenize\n\t\t", len(freq), "\t\t\t|\t\t", len( nltk_frequency_analysis(joined_text) ))
+# #print( "number of tokens found :\n\tby naive frequency analysis\t|\tby nltk's word_tokenize\n\t\t", len(freq), "\t\t\t|\t\t", len( nltk_frequency_analysis(joined_text) ))
 
-cardinal = 20
-print('here are the ', cardinal, ' most common words from ', source_filename, ':')
+# cardinal = 20
+# print('here are the ', cardinal, ' most common words from ', source_filename, ':')
 
-count=1
-for el in sorted(freq, key=freq.__getitem__, reverse=True):
-    if count <= cardinal:
-        print('\t', el, '\t --> ', freq[el], ' occurences')
-        count += 1
-    else:
-        break
+# count=1
+# for el in sorted(freq, key=freq.__getitem__, reverse=True):
+#     if count <= cardinal:
+#         print('\t', el, '\t --> ', freq[el], ' occurences')
+#         count += 1
+#     else:
+#         break
 
-tagged_text = nltk.pos_tag( word_tokenize(joined_text) )
-tag_counter = defaultdict(int)
-for (word, tag) in tagged_text:
-    tag_counter[tag] += 1
+# print("tokenizing...", end='')
+# sys.stdout.flush()
+# tokens = word_tokenize(joined_text)
+# print("done\npos tagging...", end='')
+# sys.stdout.flush()
+# tagged_text = nltk.pos_tag( tokens )
+# print('done')
+# tag_counter = defaultdict(int)
+# for (word, tag) in tagged_text:
+#     tag_counter[tag] += 1
 
-print('\n\noccurence of tags:')
-for el in sorted(tag_counter, key=tag_counter.__getitem__, reverse=True):
-    print('\t', el, '\t --> ', tag_counter[el])
+# print('\noccurence of tags:')
+# for el in sorted(tag_counter, key=tag_counter.__getitem__, reverse=True):
+#     print('\t', el, '\t --> ', tag_counter[el])
