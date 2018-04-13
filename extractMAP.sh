f1f6418294a1ec4aa4e27f7a6f4b8aa1e51f54fd
mooss <mooss@protonmail.com>
2018-04-13 16:06:02 +0200

    Améliorations du mode debug

--- extractMAP.sh
+++ extractMAP.sh
@@ -5,7 +5,12 @@ if echo $prediction | grep --quiet "2016"
 then
     reference=scorer/SemEval2016-Task3-CQA-QL-test.xml.subtaskB.relevancy
 else
-    reference=scorer/SemEval2017-Task3-CQA-QL-test.xml.subtaskB.relevancy
+    if echo $prediction | grep --quiet "2017"
+    then
+        reference=scorer/SemEval2017-Task3-CQA-QL-test.xml.subtaskB.relevancy
+    else
+        reference=scorer/SemEval2016-debug.relevancy
+    fi
 fi
 
 python2 scorer/ev.py $reference $prediction | grep "^MAP" | sed 's/ \+/;/g' | cut -f 4 -d ';'
