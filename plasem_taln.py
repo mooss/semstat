2cd3ae2ae0f4381d8e7ce978635bfe28d586cea3
mooss <mooss@protonmail.com>
2018-05-02 09:47:01 +0200

    Déplacement des fonctions make_*document_tree

--- plasem_taln.py
+++ plasem_taln.py
@@ -6,37 +6,6 @@ from collections import Counter, defaultdict
 from semeval_xml import get_semeval_id, get_related_threads, xmlextract
 from plasem_algostruct import save_object, load_object
 
-def make_document_tree(original_questions, model, content_extractor):
-    result = {}
-    for org in original_questions:
-        orgid = get_semeval_id(org)
-        result[orgid] = {
-            get_semeval_id(rel): model(content_extractor(rel))
-            for rel in get_related_threads(org)
-        }
-        result[orgid]['org'] = model(content_extractor(org))
-    return result
-
-def make_or_load_document_tree(xml_source, saved_path, model, content_extractor, verbose=False):
-    if os.path.isfile(saved_path):
-        if verbose:
-            print('Loading document tree from', saved_path)
-        result = load_object(saved_path)
-        return result
-    else:
-        if verbose:
-            print('Creating document tree. This might take a while...')
-
-        extractor = xmlextract(xml_source)
-        result = make_document_tree(
-            extractor.get_org_elements(), model, content_extractor)
-
-        if verbose:
-            print('Saving document tree to', saved_path)
-        save_object(result, saved_path)
-
-        return result
-
 def term_frequencies(bag):
     documentlen = sum(bag.values())
     return {
--- semeval_executable.py
+++ semeval_executable.py
@@ -154,6 +154,37 @@ all_indicators.update(ner_indicators)
 def getindicatorfunctions(key):
     return (wordextractors[all_indicators[key][0]], sentenceextractors[all_indicators[key][1]])
 
+def make_document_tree(original_questions, model, content_extractor):
+    result = {}
+    for org in original_questions:
+        orgid = get_semeval_id(org)
+        result[orgid] = {
+            get_semeval_id(rel): model(content_extractor(rel))
+            for rel in get_related_threads(org)
+        }
+        result[orgid]['org'] = model(content_extractor(org))
+    return result
+
+def make_or_load_document_tree(xml_source, saved_path, model, content_extractor, verbose=False):
+    if os.path.isfile(saved_path):
+        if verbose:
+            print('Loading document tree from', saved_path)
+        result = load_object(saved_path)
+        return result
+    else:
+        if verbose:
+            print('Creating document tree. This might take a while...')
+
+        extractor = xmlextract(xml_source)
+        result = make_document_tree(
+            extractor.get_org_elements(), model, content_extractor)
+
+        if verbose:
+            print('Saving document tree to', saved_path)
+        save_object(result, saved_path)
+
+        return result
+
 training_file = 'SemEval2016-Task3-CQA-QL-train-part1.xml'
 
 training_doctree = make_or_load_document_tree(
